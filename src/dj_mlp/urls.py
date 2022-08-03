b5a9ed796bcad11cd9c0e3d74a174fea6ffe7610
nishant hegde <nishant.arun.hegde@gmail.com>
2022-08-03 04:47:49 -0700

    testing drop down with js

--- src/dj_mlp/urls.py
+++ src/dj_mlp/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('performance/', include('products.urls', namespace='products'))
+    path('performance/', include('products.urls', namespace='products')),
+    path('', RedirectView.as_view(url='/performance/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/products/views.py
+++ src/products/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Purchase
+import pandas as pd
 
 
 # Create your views here.
 def chart_select_view(request):
-    return render(request, 'products/main.html', {})
+    product_df = pd.DataFrame(Product.objects.all().values())
+    purchase_df = pd.DataFrame(Purchase.objects.all().values())
+    product_df['product_id'] = product_df['id']
+    df = pd.merge(purchase_df, product_df, on='product_id').drop(['id_y', 'date_y'], axis=1) \
+        .rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+    context = {
+        'products': product_df.to_html(),
+        'purchase': purchase_df.to_html(),
+        'df': df.to_html(),
+    }
+    print(context)
+    return render(request, 'products/main.html', context)
