a72920b7b266f8184fe648289a1f583de3593358
Bob Belderbos <bobbelderbos@gmail.com>
2022-08-04 18:11:56 +0200

    renamed column, add comment re fk

--- src/import_csv/models.py
+++ src/import_csv/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,7 +7,7 @@ from django.db import models
 class Csv(models.Model):
     file_name = models.FileField(max_length=100)
     uploaded = models.DateTimeField(auto_now_add=True)
-    activated = models.BooleanField(default=False)
+    success = models.BooleanField(default=False)
 
     def __str__(self):
         return "File id: {}".format(self.id)
@@ -16,6 +17,11 @@ class Movie(models.Model):
     movie_id = models.CharField(max_length=100)
     movie_name = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE,
+    #                         null=True, blank=True)
 
     def __str__(self):
         return "Move name: {}".format(self.movie_name)
+
+
+# TODO: MovieUser m2m table
