2af7fedfeabe46af404cf79855826ec9cfa0d42e
Bob Belderbos <bobbelderbos@gmail.com>
2022-08-04 18:12:26 +0200

    remove commented code

--- src/import_csv/models.py
+++ src/import_csv/models.py
@@ -17,8 +17,6 @@ class Movie(models.Model):
     movie_id = models.CharField(max_length=100)
     movie_name = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #user = models.ForeignKey(User, on_delete=models.CASCADE,
-    #                         null=True, blank=True)
 
     def __str__(self):
         return "Move name: {}".format(self.movie_name)
